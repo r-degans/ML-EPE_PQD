15bd80b6785f87fa3aa218d9e7fbd103919c02b8
zoenzt <zoe.nezet3@gmail.com>
2025-10-24 09:18:23 +0200

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -20,14 +20,12 @@ def load_dataset(dir):
 
 file_path = "Files/"
 noise_levels = ["SNR_20db", "SNR_30db", "SNR_40db", "SNR_50db", "SNR_noiseless"]
-
 results={}
 
 for snr in noise_levels:
   data = load_dataset(os.path.join(file_path, snr))
   X = data.iloc[:, :-1]
   y = data["Label"]
-
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
   ## Model Training
@@ -38,7 +36,6 @@ for snr in noise_levels:
   ## Performance Metrics
   accuracy = accuracy_score(y_test, y_pred)
   results[snr] = accuracy
-
   print(f"\nDecision Tree (SNR value = {snr})")
   pqd = sorted(y_test.unique())
   f1 = f1_score(y_test, y_pred, average = None, labels = pqd)
